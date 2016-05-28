66e584a8d641ad8c5be173c2013b34aa2314fda6
bberenz <thebrettberenz@gmail.com>
2016-05-28 03:36:01 -0400

    Correct issues with last commit

--- sgtfrog.dev.js
+++ sgtfrog.dev.js
@@ -5,7 +5,7 @@
 // @description  SteamGifts.com user controlled enchancements
 // @icon         https://raw.githubusercontent.com/bberenz/sgtfrog/master/keroro.gif
 // @include      *://*.steamgifts.com/*
-// @version      0.3.2
+// @version      0.3.2.1
 // @downloadURL  https://raw.githubusercontent.com/bberenz/sgtfrog/master/sgtfrog.user.js
 // @updateURL    https://raw.githubusercontent.com/bberenz/sgtfrog/master/sgtfrog.meta.js
 // @require      https://code.jquery.com/jquery-1.12.3.min.js
@@ -54,7 +54,7 @@ var croak = {
 
 // Calls //
 var frog = {
-  debug: -2, //0 - off
+  debug: 0, //0 - off
   /************************************************************************LOGGING****/
   logging: {
     debug: function(message) {
